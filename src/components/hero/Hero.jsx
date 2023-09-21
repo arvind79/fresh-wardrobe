b4071f920f553ab9598f78ff02db2eccac619d69
arvind79 <81362519+arvind79@users.noreply.github.com>
2023-09-21 15:52:20 +0530

    completed and designed navbar and hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,6 +2,7 @@ import './style.css';
 import largeStar from '../../assets/images/large-star.svg';
 import coverModel from '../../assets/images/cover-model.png'
 import smallStar from '../../assets/images/small-star-3.svg';
+import { FiArrowUpRight } from "react-icons/fi";
 
 const Hero = () => {
   return (
@@ -11,17 +12,25 @@ const Hero = () => {
         <h1 className="title-2">2022</h1>
         <h1 className="title-3">Look</h1>
       </div>
+
       <div className="model-container">
         <div className="model">
           <img src={largeStar} className='large-star' alt="large star img" />
           <img src={coverModel} className='model-img' alt="cover model img" />
         </div>
       </div>
+
       <div className="orange-box"></div>
       <div className="blur-box"></div>
+
       <div className="line-star-container">
         <div className="line"></div>
         <img src={smallStar} className='small-star' alt="small star" />
+      </div>
+
+      <div className='btn-container'>
+        <button className='see-more'>See More</button>
+        <FiArrowUpRight />
       </div>
     </div>
   )
